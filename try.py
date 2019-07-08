0f834731db855bc947f8a652de1ff5b410f6cee9
Daniel Castro <daniel.castro@critigen.com>
2019-07-07 20:23:11 -0700

    Complete activity02

--- try.py
+++ try.py
@@ -9,6 +9,6 @@ from stream_exercise import StreamProcessor
 
 value = sys.argv[1]
 
-my_stream_processor = StreamProcessor(io.StringIO(value))
+my_stream_processor = StreamProcessor(io.BytesIO(value))
 result = my_stream_processor.process()
 print("Processed {} and got {}".format(value, result))
